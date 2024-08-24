c5ce57530cd56723ce03c0c1cd7f1b8a18425b3c
rinaldisantiago <santiago.rinaldi@itscordoba.edu.ar>
2024-08-24 19:24:34 -0300

    Creacion de rutas update, search y delete

--- routes/personaRoutes.js
+++ routes/personaRoutes.js
@@ -7,5 +7,8 @@ router.get("/", personaController.home);
 router.get("/list", personaController.list);
 router.post("/create", personaController.create);
 router.get("/info/:id", personaController.findById);
+router.post("/update/:id", personaController.update);
+router.get("/search", personaController.buscarPorNombre);
+router.delete("/delete/:id", personaController.deletePersona);
 
 module.exports = router; //*El module.exports hace que podamos exportar la constante router
